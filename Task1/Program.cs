a52451f77391315611e2f22e42c7af6fa6e0a93c
Oxygen <grandnorik@mail.ru>
2022-11-20 17:35:17 +0600

    step1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -5,3 +5,41 @@ m = 3, n = 4.
 8 7,8 -7,1 9
 */
 
+int GetNumber(string msg)
+{
+    Console.Write(msg);
+    int userNumber = Convert.ToInt32(Console.ReadLine());
+    return userNumber;
+}
+
+double[,] InitArray(int m, int n)
+{
+    Random rnd = new Random();
+    double[,] array = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = rnd.Next(-10, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int n = GetNumber("Задайте количество строк: ");
+int m = GetNumber("Задайте количество столбцов: ");
+double[,] array = InitArray(m, n);
+PrintArray(array);
+
