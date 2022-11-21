97f41ba6a1703dc825722cb7369304a73b086ad0
Oxygen <grandnorik@mail.ru>
2022-11-21 17:48:13 +0600

    Task2 done

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -7,3 +7,47 @@
 17 -> такого числа в массиве нет
 */
 
+int UserNumber(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int[,] InitArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = rnd.Next(0, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int rows = UserNumber("Введите индекс строки: ");
+int columns = UserNumber("Введите индекс столбца: ");
+int[,] array = InitArray(4, 4);
+PrintArray(array);
+
+if (rows < array.GetLength(0) && columns < array.GetLength(1))
+Console.WriteLine($"Это число {array[rows, columns]}.");
+else 
+Console.WriteLine($"Числа с координатами [{rows}, {columns}] в массиве нет.");
+
