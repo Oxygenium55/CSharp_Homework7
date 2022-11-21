52c79be5818f3badbedd9d84b1053ef914468889
Oxygen <grandnorik@mail.ru>
2022-11-21 18:03:32 +0600

    Task3 done

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -7,3 +7,52 @@
 Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 */
 
+int UserNumber(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int[,] InitArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = rnd.Next(0, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine("---------------------------------------");
+}
+
+int m = UserNumber("Задайте количество строк: ");
+int n = UserNumber("Задайте количество столбцов: ");
+int[,] array = InitArray(m, n);
+PrintArray(array);
+Console.WriteLine("Среднее арифметическое каждого столбца: ");
+
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum += array[i, j];
+    }
+    Console.Write($"{sum / array.GetLength(0)}; ");
+}
